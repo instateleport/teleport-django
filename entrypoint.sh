7aea9c2841e1de8b1a6c2128a6b39585aa419dd3
Ivan <BaHbka35@gmail.com>
2022-10-27 22:04:24 +0700

    small fix in entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 sleep 5
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
 gunicorn -b 0.0.0.0:8000 -w 1 getsub.wsgi:application & \
     celery -A getsub worker -l info & \
     celery -A getsub beat -l info
