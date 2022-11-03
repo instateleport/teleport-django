fd550b7f1722bbd505fd90abfb5fbd3d2f77d4f7
Ivan <BaHbka35@gmail.com>
2022-11-03 17:18:36 +0700

    fix entrypoing.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm celerybeat*
 gunicorn -b $GUNICORN_HOST:$GUNICORN_PORT -w $GUNICORN_WORKERS_AMOUNT getsub.wsgi:application & \
     celery -A getsub worker -l info & \
     celery -A getsub beat -l info
