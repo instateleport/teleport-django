88ee5a33c40f15fc41c3fa008314ee28c23b4cf0
NetBiom <110700797+TZbooo@users.noreply.github.com>
2022-12-11 05:41:35 +0300

    change

--- teleport/getsub/settings.py
+++ teleport/getsub/settings.py
@@ -218,4 +218,4 @@ CACHES = {
 }
 CACHE_TTL = 60 * 10
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
